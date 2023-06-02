b8bdfd68bc18ca9fccafed7fdf3d46ff97544f2d
kyu <kncljmmmm@gmail.com>
2023-06-02 21:13:15 +0900

    리엑트 2-7

--- src/App.js
+++ src/App.js
@@ -4,52 +4,95 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import data from './date.js';
 import { useState } from 'react';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import { SubPage } from './routes/Page';
+import userEvent from '@testing-library/user-event';
 
 
 
 function App() {
-  
-  let [shoes] = useState(data)
+
+  let [shoes] = useState(data);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
+
+
+
+
+
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">쇼핑몰</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/sub') }}>Card</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className='main-bg'></div>
 
-      <div className="container">
-        <div className="row">
 
-            {
-              shoes.map((a, i)=>{
-                return(
-                <Card shoes={shoes[i]} i={i}/>
-                )
-              })
-            }
-        </div>
-      </div>
+
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+
+        <Route path='/sub/:id' element={<SubPage shoes={shoes} />} />
+    
+
+        <Route path='/about' element={<About />}> 
+          <Route path='member' element={<div>맴버임</div>} />
+          <Route path='location' element={<div> 위치 정보임 </div>} />
+        </Route>
+
+        <Route path='*' element={<div>404 Error</div>} />
+
+        
+      </Routes>
+
     </div>
   );
 
-  function Card(props){
+  function About(){
+    return (
+      <div>
+        <h4>회사정보임</h4>
+        <Outlet></Outlet>
+      </div>
+    )
+  }
+
+  function Card(props) {
     return (
       <div className="col-md-4">
-        <img src={'https://codingapple1.github.io/shop/shoes' + (props.i+1) +'.jpg'} width="80%" />
-        <h4>{props.shoes.title }</h4>
+        <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="80%" />
+        <h4>{props.shoes.title}</h4>
         <p>{props.shoes.content}</p>
       </div>
     )
   }
+
+  function MainPage() {
+    return (
+      <>
+        <div className='main-bg'></div>
+        <div className="container">
+          <div className="row">
+            {shoes.map((a, i) => {
+              return (<Card shoes={shoes[i]} i={i} />
+              )
+            })}
+          </div>
+        </div>
+      </>
+
+
+    )
+  }
+
+
+
 }
 
 
