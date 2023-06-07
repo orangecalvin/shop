f2ebac344021a3afe8a24deceddf4a1e6e845c78
kyu <kncljmmmm@gmail.com>
2023-06-07 23:01:45 +0900

    ul 업데이트

--- src/routes/Page.js
+++ src/routes/Page.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 
 function SubPage(props) {
   //테스트
-  const [inputValue, setInputValue] = useState('');
-  const [showWarning, setShowWarning] = useState(false);
-  let [count, setCount] = useState(0)
-  let [alert, setAlert] = useState(true)
+
+  let [count, setCount] = useState(0);
+  let [alert, setAlert] = useState(true);
+  let [탭, 탭변경] = useState(0);
+
   let { id } = useParams();
   let 찾은상품 = props.shoes.find(function (x) {
     return x.id == id
@@ -20,17 +22,7 @@ function SubPage(props) {
     }
   }, [])
 
-  const handleChange = (event) => {
-    const value = event.target.value;
-    setInputValue(value);
 
-    // 숫자 이외의 문자가 포함되어 있는지 검사
-    if (/\D/.test(value)) {
-      setShowWarning(true); // 경고 메시지 표시
-    } else {
-      setShowWarning(false); // 경고 메시지 숨김
-    }
-  };
 
   return (
     <div className="container">
@@ -48,19 +40,40 @@ function SubPage(props) {
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
         </div>
         <div className="col-md-6">
-          <div>
-            <input type="text" value={inputValue} onChange={handleChange} />
-            {showWarning && <div>숫자 이외의 문자가 입력되었습니다!</div>}
-          </div>
+
           <h4 className="pt-5">{찾은상품.title}</h4>
           <p>{찾은상품.content}</p>
           <p>{찾은상품.price}원</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
+        <Nav variant="tabs" defaultActiveKey="link0">
+          <Nav.Item>
+            <Nav.Link  onClick={()=>{ 탭변경(0) }} eventKey="link0">버튼0</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={()=>{ 탭변경(1) }} eventKey="link1">버튼1</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link  onClick={()=>{ 탭변경(2) }} eventKey="link2">버튼2</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <TabContent 탭={탭} />
+        
+      
+      
+
       </div>
     </div>
   )
 }
+
+
+function TabContent({탭}){
+  return [ <div>내용0</div>, <div>내용1</div>, <div>내용2</div> ][탭]
+}
+
+
+
 
 
 export { SubPage }
