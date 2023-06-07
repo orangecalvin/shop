5599c9bc1ddf864eac2b58ebe8e570923870a5b2
kyu <kncljmmmm@gmail.com>
2023-06-07 23:23:16 +0900

    리엑트 애니메이션

--- src/routes/Page.js
+++ src/routes/Page.js
@@ -69,7 +69,21 @@ function SubPage(props) {
 
 
 function TabContent({탭}){
-  return [ <div>내용0</div>, <div>내용1</div>, <div>내용2</div> ][탭]
+
+  let [fade, setFade] = useState('')
+
+  useEffect(()=>{
+    let a = setTimeout(()=>{setFade('end')}, 100)
+    return ()=>{
+    clearTimeout(a)
+    setFade('')
+    }
+  },[탭])
+  return (
+    <div className={`start ${fade}`}>
+      { [<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][탭] }
+    </div>
+  )
 }
 
 
